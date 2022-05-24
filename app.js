560a9dcac263c1567df65ceadf54c93f7d73efa3
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-05-24 15:37:22 +0200

    Connected to mongodb-server

--- app.js
+++ app.js
@@ -17,4 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//Importera mongodb
+const MongoClient = require("mongodb").MongoClient;
+
+//Connecta till mongodb server
+MongoClient.connect("mongodb://localhost:27017", {
+    useUnifiedTopology: true
+})
+.then(client => {
+    console.log("Vi är uppkopplade mot mongodb-databasen");
+
+    const db = client.db("newsletterlist");
+    app.locals.db = db;
+})
+
 module.exports = app;
